ded3c006f64da1e6110a2726f3b452eac1bc85dd
Alexander Colin Starr <46503375+AStarr649@users.noreply.github.com>
2020-10-12 12:11:46 -0500

    Add files via upload

---
+++ Group_Project/Group_Project/FormClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project
+{
+    class FormClass
+    {
+        public static LoginForm sf = new LoginForm();
+        public static InfoForm infoF = new InfoForm();
+        public static MenuForm menuF = new MenuForm();
+        public static Char_CreationForm charF = new Char_CreationForm();
+        public static char_ClassesForm cClassF = new char_ClassesForm();
+        public static char_RacesForm cRaceF = new char_RacesForm();
+    }
+}
---
+++ Group_Project/Group_Project/char_RacesForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_Project
+{
+    public partial class char_RacesForm : Form
+    {
+        public char_RacesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            FormClass.charF.char_Race = lbxRaces.SelectedIndex.ToString();
+            this.Close();
+            FormClass.charF.Enabled = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormClass.charF.Enabled = true;
+        }
+    }
+}
