0784432ce5c86ff57b8330e606963ea54edf6009
Alexander Colin Starr <46503375+AStarr649@users.noreply.github.com>
2020-10-20 09:42:53 -0500

    Update char_ClassesForm.cs

--- Group_Project/Group_Project/char_ClassesForm.cs
+++ Group_Project/Group_Project/char_ClassesForm.cs
@@ -29,5 +29,42 @@ namespace Group_Project
             this.Close();
             FormClass.charF.Enabled = true;
         }
+        
+        private void lbxClasses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (lbxClasses.SelectedIndex)
+            {
+                case 0:
+                    lblClassDesc.Text = "Physically power, often acting without thinking.";
+                    break;
+                case 1:
+                    lblClassDesc.Text = "Strong, heavily armored class, the 'tank' of battles.";
+                    break;
+                case 2:
+                    lblClassDesc.Text = "Spiritualist capable of restorative magicks and enchancements.";
+                    break;
+                case 3:
+                    lblClassDesc.Text = "Weavers of elemental magicks, preferences to elemental affinity.";
+                    break;
+                case 4:
+                    lblClassDesc.Text = "Masters of tracking and ranged weaponry.";
+                    break;
+                case 5:
+                    lblClassDesc.Text = "Deceptive, under-handed, can sneak or talk their way out of most situations.";
+                    break;
+                case 6:
+                    lblClassDesc.Text = "Deadly ambush assassins with either melee or aranged weapons.";
+                    break;
+                case 7:
+                    lblClassDesc.Text = "Proficient in combining elemental magicks with specialized ranged-weaponry.";
+                    break;
+                case 8:
+                    lblClassDesc.Text = "One in tune with several elements to provide support and healing.";
+                    break;
+                default:
+                    lblClassDesc.Text = "";
+                    break;
+            }
+        }
     }
 }
